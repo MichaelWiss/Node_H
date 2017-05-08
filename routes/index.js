28d5d555145e41e16d81a88b8a5fbface9365f4c
MichaelWiss <michael.wiss@gmail.com>
2017-05-08 17:23:12 -0500

    routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  // res.json(Michael);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render();
+});
+
+router.get('/reverse/:name', (req, res) => {
+	const reverse = [...req.params.name].reverse().
+	  join('');
+    res.send(reverse);
 });
 
 module.exports = router;
