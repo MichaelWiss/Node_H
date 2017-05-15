0099c8d68af62c317bcb8343301a1876a7c50597
MichaelWiss <michael.wiss@gmail.com>
2017-05-15 18:27:06 -0500

    autocomplete credentials

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,14 +1,12 @@
 function autocomplete(input, latInput, lngInput) {
-    console.log(input, latInput, lngInput);
-    if(!input) return; 
-    const dropdown = new google.maps.places.Autocomplete(input);
+  if(!input) return; // skip this fn from running if there is not input on the page
+  const dropdown = new google.maps.places.Autocomplete(input);
 
-    dropdown.add.Listener('place_changed', () => {
-       const place = dropdown.getPlace();
-       console.log(place);
-       latInput.value = place.geometry.location.lat();
-       lngInput.value = place.geometry.location.lng();
-    });
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
 }
 
 export default autocomplete;
