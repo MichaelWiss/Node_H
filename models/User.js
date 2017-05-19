686920c000d363186d6e2a86571aa032f24c8992
MichaelWiss <michael.wiss@gmail.com>
2017-05-19 13:59:42 -0500

    User imports

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-mongoose.Promise
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongoErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('password-local-mongoose');
