0825b155acceb08b17251754f97710c632753ac0
MichaelWiss <michael.wiss@gmail.com>
2017-05-19 12:36:36 -0500

    login page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
