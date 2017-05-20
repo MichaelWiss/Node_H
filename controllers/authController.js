28bf7e15ce440e36351a056724016c797b3d099b
MichaelWiss <michael.wiss@gmail.com>
2017-05-20 16:47:04 -0500

    success login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,5 +3,6 @@ const passport = require('passport');
 exports.login = passport.authenticate('local', {
    failureRedirect: '/login',
    failureFlash: 'Failed Login',
-   
+   successRedirect: '/',
+   successFlash: 'You are now logged in!'
 });
