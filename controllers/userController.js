4f35e7efee4dce5f28876b84e8ef4c13b2a730ec
MichaelWiss <michael.wiss@gmail.com>
2017-05-20 15:52:12 -0500

    using promisify for passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,6 @@ exports.validateRegister = (req, res, next) => {
 
 exports.register = async (req, res, next) => {
 	const user = new User({ email: req.body.email, name: req.body.name });
-	
-   
+    const register = promisify(User.register, User);       
+    await register(user, req.body.password);
 };
