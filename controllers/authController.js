d0a773ce5dce37c203a14c113ed1a464e0ac8cdd
MichaelWiss <michael.wiss@gmail.com>
2017-05-23 16:12:44 -0500

    crypto

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const crypto = require('crypto');
 
 exports.login = passport.authenticate('local', {
    failureRedirect: '/login',
@@ -21,3 +22,16 @@ exports.isLoggedIn = (req, res, next) => {
 	req.flash('error', 'Oops you must be logged in to do that!');
 	res.redirect('/login');
 };
+
+exports.forgot = async (req, res) => {
+	//see if a user with that email exists
+	const user = await User.findOne({ email: req.body.email });
+	if(!user) {
+		req.flash('error', 'No account with that email exists');
+		return res.redirect('/login');
+	}
+
+	user.resetPasswordToken = 
+	//set reset tokens and expiry on their account
+	user.reset
+}
