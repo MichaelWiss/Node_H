7d824f75b24eceba19d4386cd55adb0628f72f0b
MichaelWiss <michael.wiss@gmail.com>
2017-05-23 15:41:43 -0500

    login, forgot views

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,6 +54,6 @@ exports.updateAccount = async (req, res) => {
        { $set: updates },
        { new: true, runValidators: true, context: 'query'}
 	);
-
-	res.json(user);
+    req.flash('success', 'Updated the profile!');
+	res.redirect('back');
 };
