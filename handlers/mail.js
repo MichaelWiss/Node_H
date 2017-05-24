34abc572b8a3d7154d6cfbb1c29003c724ef281b
MichaelWiss <michael.wiss@gmail.com>
2017-05-24 16:15:29 -0500

    mail transport

--- handlers/mail.js
+++ handlers/mail.js
@@ -1,1 +1,14 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const juice = require('juice');
+const htmlToText = require('htmlToText');
+const promisify = require('es6-promisify');
+
+const transport = nodemailer.createTransport({
+	host: process.env.MAIL_HOST,
+	port: process.env.MAIL_PORT,
+	auth: {
+		user: process.env.MAIL_USER,
+		pass: process.env.MAIL_PASS
+	}
+})
