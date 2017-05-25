1baa4664f01ca62a10680128a773ce8d08497659
MichaelWiss <michael.wiss@gmail.com>
2017-05-24 21:37:54 -0500

    store owner equals user.id

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -47,6 +47,7 @@ exports.resize = async (req, res, next) => {
 
 
 exports.createStore = async (req, res) => {
+	req.body.author = req.user._id;
 	const store = await (new Store(req.body)).save();
 	req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
 	res.redirect(`/store/${store.slug}`);
@@ -59,11 +60,19 @@ exports.getStores = async (req, res) => {
 	res.render('stores', { title: 'Stores', stores });
 };
 
+
+const confirmOwner = (store, user) => {
+   if (!store.author.equals(user._id)) {
+   	  throw Error('You must own a store in order to edit it!');
+   }
+};
+
 exports.editStore = async (req, res) => {
 	//find store
 	const store = await Store.findOne({_id: req.params.id });
 	
 	//confirm they are the owner
+	confirmOwner(store, )
 	//render out edit form
 	res.render('editStore',  { title: `Edit ${store.name}`, store });
 }
@@ -82,7 +91,8 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res) => {
-	const store = await Store.findOne({ slug: req.params.slug });
+	const store = await Store.findOne({ slug: req.params.slug })
+	.populate('author');
 	if (!store) return next();
 	res.render('store', { store, title: store.name });
 };
--- models/Store.js
+++ models/Store.js
@@ -35,7 +35,8 @@ const storeSchema = new mongoose.Schema({
    photo: String,
    author: {
       type: mongoose.Schema.ObjectId,
-      ref: 'User'
+      ref: 'User',
+      required: 'You must supply an author'
    }
 });
 
