2bfaedd1d7f7df9bd022daab99416da4de8aa7b1
MichaelWiss <michael.wiss@gmail.com>
2017-05-29 17:01:38 -0500

    markers

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -987,8 +987,10 @@ exports.$$ = $$;
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+   value: true
 });
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 var _axios = __webpack_require__(2);
 
@@ -999,21 +1001,42 @@ var _bling = __webpack_require__(10);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mapOptions = {
-    center: { lat: 43.2, lng: -79.8 },
-    zoom: 8
+   center: { lat: 43.2, lng: -79.8 },
+   zoom: 8
 };
 
 function loadPlaces(map) {
-    var lat = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 43.2;
-    var lng = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : -79.8;
+   var lat = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 43.2;
+   var lng = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : -79.8;
+
+   _axios2.default.get('/api/stores/near?lat=' + lat + '&lng=' + lng).then(function (res) {
+      var places = res.data;
+      if (!places.length) {
+         alert('no places found!');
+         return;
+      }
+
+      var markers = places.map(function (place) {
+         var _place$location$coord = _slicedToArray(place.location.coordinates, 2),
+             placeLng = _place$location$coord[0],
+             placeLat = _place$location$coord[1];
+
+         var position = { lat: placeLat, lng: placeLng };
+         var marker = new google.maps.Marker({ map: map, position: position });
+         marker.place = place;
+         return marker;
+      });
+      console.log(markers);
+   });
 }
 
 function makeMap(mapDiv) {
-    if (!mapDiv) return;
-    var map = new google.maps.Map(mapDiv, mapOptions);
-    var input = (0, _bling.$)('[name="geolocate"]');
-    console.log(input);
-    var autocomplete = new google.maps.places.Autocomplete(input);
+   if (!mapDiv) return;
+   var map = new google.maps.Map(mapDiv, mapOptions);
+   loadPlaces(map);
+
+   var input = (0, _bling.$)('[name="geolocate"]');
+   var autocomplete = new google.maps.places.Autocomplete(input);
 };
 
 exports.default = makeMap;
