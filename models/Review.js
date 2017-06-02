a7452d9c4998bdae31cf6d72d4b4a6caf23c1be4
MichaelWiss <michael.wiss@gmail.com>
2017-06-02 11:58:44 -0500

    reviewForm

--- models/Review.js
+++ models/Review.js
@@ -7,11 +7,24 @@ const reviewSchema = new mongoose.Schema({
 		defautl: Date.now
 	}
 	author: {
-		
+		type: mongoose.Schema.ObjectId,
+		ref: 'User',
+		required: 'You must supply an author!'
+	},
+	store: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Store',
+		required: "You must supply a store!"
+	},
+	text: {
+		type: String,
+		required: 'Your review must have a text!'
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 5
 	}
-	store
-	text
-	rating
 
 });
 
