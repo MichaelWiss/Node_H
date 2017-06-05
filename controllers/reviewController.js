2db42c6c1ab2e315755dd1f194ebf601f9426902
MichaelWiss <michael.wiss@gmail.com>
2017-06-05 10:05:22 -0500

    star reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,6 @@
-const mongoose = required('mongoose');
+const mongoose = require('mongoose');
 const Review = mongoose.model('Review');
 
 exports.addReview = async (req, res) => {
-	
+	res.json(req.body);
 }
