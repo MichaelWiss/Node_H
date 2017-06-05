80bc61eae733b9c7c26a67d9a6b6a2379b39599c
MichaelWiss <michael.wiss@gmail.com>
2017-06-05 10:33:38 -0500

    newReview Const

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,5 +4,5 @@ const Review = mongoose.model('Review');
 exports.addReview = async (req, res) => {
 	req.body.author = req.user._id;
 	req.body.store = req.params.id;
-	res.json(req.body);
+	const newReview = new Review(req.body);
 }
