527ccc7e592b94c75be6a343bb625a698214c925
MichaelWiss <michael.wiss@gmail.com>
2017-06-05 10:42:04 -0500

    save review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,4 +5,7 @@ exports.addReview = async (req, res) => {
 	req.body.author = req.user._id;
 	req.body.store = req.params.id;
 	const newReview = new Review(req.body);
+	await newReview.save();
+	req.flash('success', 'Review Saved');
+	res.redirect('back');
 }
