c646205dd9527a000c6abfc10b5bd2ad43e2f2ae
MichaelWiss <michael.wiss@gmail.com>
2017-06-09 13:54:53 -0500

    next

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -64,7 +64,8 @@ exports.getStores = async (req, res) => {
   const stores = await Store
   .find()
   .skip(skip)
-  .limit(limit)
+  .limit(limit);
+  
   res.render('stores', { title: 'Stores', stores });
 };
 
